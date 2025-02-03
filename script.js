9aa5e5a4a04d09cfeaa4f20378bb81733d64dda6
Anayet hossain <anayethossain030@gmail.com>
2025-02-03 17:06:36 +0600

    shopping cart plus minus action added

---
+++ script.js
@@ -0,0 +1,22 @@
+function handleProductChange(product,isIncrease) {
+    const productInput = document.getElementById(product+"-count");
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
+    if(isIncrease == true) {
+        productNewCount = productCount + 1;
+    }  
+    if(isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
+    }  
+    productInput.value = productNewCount;
+    let productTotal = 0;
+    if(product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+
+
+    document.getElementById(product+"-total").innerText = "$" + productTotal;
+}
