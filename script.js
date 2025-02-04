d3b57d15697aaad47e3626a491d4e5cbc582c212
Anayet hossain <anayethossain030@gmail.com>
2025-02-04 10:25:32 +0600

    total,tax & grandtotal option added

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ function handleProductChange(product,isIncrease) {
     const productCount = parseInt(productInput.value);
     let productNewCount = productCount;
     if(isIncrease == true) {
-        productNewCount = productCount + 1;
-    }  
+        productNewCount = productCount + 1 ;
+    }
     if(isIncrease == false && productCount > 0) {
         productNewCount = productCount - 1;
-    }  
-    productInput.value = productNewCount;
+    }
+    productInput.value = productNewCount
     let productTotal = 0;
-    if(product == 'phone') {
+    if(product == "phone") {
         productTotal = productNewCount * 1219;
     }
-    if(product == 'case') {
+    if(product == "case") {
         productTotal = productNewCount * 59;
     }
-
-
     document.getElementById(product+"-total").innerText = "$" + productTotal;
+    calculateTotal();
+}
+
+function calculateTotal() {
+    const phoneCount = getInputValue("phone");
+    const caseCount = getInputValue("case");   
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.getElementById('total-price').innerText = "$" + totalPrice;
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById("tax-amount").innerText = "$" + tax;
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = "$" + grandTotal;
+}
+function getInputValue(product) {
+    const productInput = document.getElementById(product+"-count");
+    const productCount = parseInt(productInput.value);
+    return productCount;
 }
